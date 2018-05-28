da3c1f929267b077a02ab0b38f7ed2fa40119f89
darc0502 <kobataka61116@yahoo.co.jp>
2018-05-28 14:42:00 +0900

    課題提出
    よろしくお願いします。

---
+++ Assets/BGM2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM2 : MonoBehaviour {
+
+	public GameObject CubePrefab;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().volume = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "cubetag" || col.gameObject.tag == "groundtag") {
+			GetComponent<AudioSource> ().volume = 0.5f;
+		}
+	}
+}
